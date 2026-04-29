0752c3a86606531821b5697bbbac14a3cf2deea6
HaseebHarry <haseebharry07@gmail.com>
2026-04-29 22:41:29 +0500

    updated code for login issue on dashboard

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ exports.postLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const response = await axios.post("http://localhost:3000/admin/login", {
+    const response = await axios.post("https://themebuilder-six.vercel.app/admin/login", {
       email,
       password
     });
